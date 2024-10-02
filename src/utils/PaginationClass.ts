cd346de0b20178d28dafd60fc5a603f8582506ab
Didar Yermekuly <yermekulyd@gmail.com>
2024-10-02 18:27:38 +0500

    PaginationClass: remove looped property

--- src/utils/PaginationClass.ts
+++ src/utils/PaginationClass.ts
@@ -1,16 +1,18 @@
 class PaginationClass {
   private currentPage: number;
   private totalPages: number;
-  private isLooped: boolean;
 
-  constructor(totalPages: number, isLooped: boolean = false) {
+  constructor(totalPages: number) {
     this.currentPage = 1;
     this.totalPages = totalPages;
-    this.isLooped = isLooped;
   }
 
   getCurrentPage() {
     return this.currentPage;
+  }
+
+  getTotalPages() {
+    return this.totalPages;
   }
 
   goToPage(page: number) {
@@ -21,34 +23,21 @@ class PaginationClass {
   nextPage() {
     if (this.currentPage < this.totalPages) {
       this.currentPage++;
-    } else if (this.isLooped) {
-      this.currentPage = 1;
     }
   }
 
   prevPage() {
     if (this.currentPage > 1) {
       this.currentPage--;
-    } else if (this.isLooped) {
-      this.currentPage = this.totalPages;
     }
   }
 
   nextPages(step: number) {
-    if (this.isLooped) {
-      this.currentPage = ((this.currentPage - 1 + step) % this.totalPages) + 1;
-    } else {
-      this.currentPage = Math.min(this.currentPage + step, this.totalPages);
-    }
+    this.currentPage = Math.min(this.currentPage + step, this.totalPages);
   }
 
   prevPages(step: number) {
-    if (this.isLooped) {
-      this.currentPage =
-        ((this.currentPage - 1 - step + this.totalPages) % this.totalPages) + 1;
-    } else {
-      this.currentPage = Math.max(this.currentPage - step, 1);
-    }
+    this.currentPage = Math.max(this.currentPage - step, 1);
   }
 }
 
