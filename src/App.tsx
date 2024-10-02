9b079997c2431cf0484915a9289f2a20a8d1d935
Didar Yermekuly <yermekulyd@gmail.com>
2024-10-02 22:09:50 +0500

    App: displaying Table and Pagination components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import Table from "./components/Table";
+import Pagination from "./components/Pagination";
+import users, { User } from "./data/mockData";
+import "./styles/App.scss";
+
+const ITEMS_PER_PAGE = 5;
 
 function App() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [currentData, setCurrentData] = useState<User[]>([]);
+
+  const totalPages = Math.ceil(users.length / ITEMS_PER_PAGE);
+
+  useEffect(() => {
+    const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
+    const endIndex = startIndex + ITEMS_PER_PAGE;
+    setCurrentData(users.slice(startIndex, endIndex));
+  }, [currentPage]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1 className="app__title">Таблица пользователей!</h1>
+      <Table data={currentData} />
+      <Pagination
+        totalPages={totalPages}
+        step={3}
+        onPageChange={(page) => setCurrentPage(page)}
+      />
     </div>
   );
 }
